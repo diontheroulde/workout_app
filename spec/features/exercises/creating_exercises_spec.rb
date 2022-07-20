cbb84aafdcd4b2b6da1ad92329e29add0813670a
Dion Theroulde <dion.theroulde@powerhrg.com>
2022-07-20 13:36:12 -0400

    Add exercise model and form partial

--- spec/features/exercises/creating_exercises_spec.rb
+++ spec/features/exercises/creating_exercises_spec.rb
@@ -14,8 +14,8 @@ RSpec.feature "Creating Exercise" do
     expect(page).to have_link("Back")
 
     fill_in "Duration", with: 70
-    fill_in "Workout Details", with: "Weight lifting"
-    fill_in "Activity Date", with: "2022-07-20"
+    fill_in "Workout details", with: "Weight lifting"
+    fill_in "Activity date", with: "2022-07-20"
     click_button "Create Exercise"
 
     expect(page).to have_content("Exercise has been created")
